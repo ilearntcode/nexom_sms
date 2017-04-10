40bc84095b1ed02059843c96a6be39c36ed07380
ilearntcode <ilearntcode@gmail.com>
2017-04-10 03:27:39 +0100

    project setup

--- server/serve.js
+++ server/serve.js
@@ -1,13 +1,22 @@
 /**
  * Module dependencies
  */
-var app = require('./app');
+var app = require('./app'),
+    socketio = require('socket.io');
 
 /**
  * Module variables
  */
 var port = process.env.port || 3000;
 
-app.listen(port, function() {
+var server = app.listen(port, function() {
     console.log('Server running on ', port);
+});
+
+var io = socketio(server);
+io.on('connection', function(socket) {
+    console.log('Socket connected');
+    socket.on('disconnect', function() {
+        console.log('Socket disconnected');
+    });
 });
